8771fda983e11dbe0504427ffb863351359b3df0
Franco Ramos <eduingle@gmail.com>
2018-07-06 22:30:53 -0600

    podemos almacenar los datos de un cliente en un archivo

--- gym.go
+++ gym.go
@@ -4,18 +4,26 @@ import (
 	"bufio"
 	"dmnuw"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strconv"
 	"time"
 )
 
+type fecha struct {
+	dia int
+	mes time.Month
+	año int
+}
+
 type cliente struct {
-	name          string
-	age           string
-	dateofpay     string
-	mounthprecius int //precio del mes
-	cellphone     string
+	name            string
+	age             string
+	dateofpay       string
+	mounthprecius   int //precio del mes
+	cellphone       string
+	dateinscription fecha
 }
 
 func limpiar() {
@@ -28,29 +36,90 @@ func dormir() {
 }
 
 func leer() string {
-	bufer, _ := bufio.NewReader(os.Stdin)
-	texto := bufer.ReadString('\n')
+	bufer := bufio.NewReader(os.Stdin)
+	texto, _ := bufer.ReadString('\n')
 
 	texto = dmnuw.Onlystring(texto)
 	return texto
 }
-func fechaActual() string {
-	r := time.Now()
-	tiempo := r.Date()
+
+func generarfechadentrode(d *fecha) {
+	s := time.Now()
+	d.año, d.mes, d.dia = s.Date() //generamos la fecha
+
+}
+
+/*func convertcliente(c cliente) (r []byte) {
+	var b []byte
+	for i, v := range c {
+		b = append(b, v)
+	}
+	b = append(b, '\n')
+	return b
+}*/
+
+/*func backup(r *os.File) {
+	f, _ := os.Create("tmp.txt")
+	sl := ioutil.ReadFile("no tocar/data.txt")
+	//convertimos a string todo lo que tenemos aqui en r
+	l := string(sl)
+	//escribimos todo en temp.txt
+	f.WriteString(l)
+
+}*/
+
+func createfiletemp() {
+	f, _ := os.Create("no tocar/tmp.txt")
+	sl, _ := ioutil.ReadFile("no tocar/data.txt")
+	//convertimos a string todo lo que tenemos aqui en r
+	l := string(sl)
+	//añadimos lo que ya tenemos en el documento
+	//escribimos todo en temp.txt
+	f.WriteString(l)
+}
+
+func añadirAarchivo(f *os.File, datoscliente cliente) {
+	j, _ := f.Stat()
+	n := j.Name()
+	//leemos todos los datos del archivo en un string
+	//firstslice, _ := ioutil.ReadFile(n)
+	//contentbeforechanges := string(sl)
+	//cerramos ese flujo acia el file
+	f.Close()
+	//abrimos un nuvo flujo acia el archivo en modo añadir
+	l, _ := os.OpenFile(n, os.O_APPEND, 0777)
+	//leemos el archivo cual es proporcionado pr
+	//sl := ioutil.ReadFile(n)
+
+	datoscliente.dateofpay = strconv.Itoa(datoscliente.dateinscription.dia)
+	l.WriteString("nombre : ")
+	l.WriteString(datoscliente.name)
+	l.WriteString(" edad: ")
+	l.WriteString(datoscliente.age)
+	l.WriteString(" cellular : ")
+	l.WriteString(datoscliente.cellphone)
+	l.WriteString(" dia de pago: ")
+	l.WriteString(datoscliente.dateofpay)
+	l.WriteString(" fecha inscripcion: ")
+	tmp := time.Now().String() + "\n"
+	l.WriteString(tmp)
+
 }
 
 func main() {
 	//creamos un flujo de lectura
 	//entradabufer := bufio.NewReader(os.Stdin)
+
 	var contador, l int = 0, 0
 	var c1 cliente
-	_, err := os.Stat("clientes.txt")
+	//var date fecha
+	_, err := os.Stat("no tocar/data.txt")
 	if err != nil {
 		if os.IsNotExist(err) {
 			//os.Create("clientes.txt")
 			os.Mkdir("no tocar", 0777)
-			os.Create("no tocar/clientes.txt")
-			os.Create("no tocar/mensualidades.txt")
+			os.Create("no tocar/data.txt")
+			//os.Create("no tocar/fechasinscripciones.txt")
 
 		}
 	}
@@ -68,7 +137,7 @@ func main() {
 
 			limpiar()
 			for l == 0 { //for clientes
-
+				fmt.Println("Clientes:\n\n")
 				respuesta = dmnuw.Parametrs("Ingresar Cliente : 1",
 					"Modificar Cliente : 2",
 					"Borrar Cliente : 3",
@@ -76,30 +145,57 @@ func main() {
 					"Regresar al menu principal : 5")
 				//convertimoa a entero la respuesta ingresdad en forma de string
 				r, _ = strconv.Atoi(respuesta)
+				fmt.Println(r)
 				switch r {
 				case 1:
 					l = 0
-					for l == 0 {
-
+					for l == 0 { //for case 1
+						createfiletemp()
 						fmt.Println("ingrese nombre del cliente")
 						c1.name = leer()
 						fmt.Println("ingrese edad del cliente")
 						c1.age = leer()
 						fmt.Println("ingrese celular del cliente")
 						c1.cellphone = leer()
-						fmt.Println("el nombre del cliente es \t ", c1.name,
-							"si desea Guardar este nombre ingrese 1")
+						fmt.Println("el nombre del cliente es \t\n ", c1.name,
+							"\n\tsi desea Guardar este nombre ingrese 1")
 						respuesta = leer()
 						r, _ = strconv.Atoi(respuesta)
 						if r == 1 {
-
-							l++
+							generarfechadentrode(&c1.dateinscription)
+							file, _ := os.OpenFile("no tocar/data.txt", os.O_APPEND|os.O_RDWR, 0777)
+							///añadirAarchivo(file, c1)
+							//inscrip, err := os.OpenFile("no tocar/fechasinscripciones.txt", os.O_RDWR, 0777)
+							/**/
+							c1.dateofpay = strconv.Itoa(c1.dateinscription.dia)
+							//Guardamos cliente pero antes guardamos toda la informacione en c1
+							file.WriteString("nombre : ")
+							file.WriteString(c1.name)
+							file.WriteString(" edad: ")
+							file.WriteString(c1.age)
+							file.WriteString(" cellular : ")
+							file.WriteString(c1.cellphone)
+							file.WriteString(" dia de pago: ")
+							file.WriteString(c1.dateofpay)
+							file.WriteString(" fecha inscripcion: ")
+							tmp := time.Now().String()
+							run := []rune(tmp)
+							//reducimos el string con append
+							run = append(run[:0], run[:20]...)
+							tmp = string(run)
+							tmp = tmp + "\n"
+							//tmp := time.Now().String() + "\n"
+							file.WriteString(tmp) /**/
+							file.Close()
+							limpiar()
+							l++ //para salir del for
+							r = 0
 						} else {
 							continue
 						}
 
-					}
-
+					} //finaliza for case 1
+					l = 0 //para que permita ingresar otra vez al menu de clientes
 				case 2:
 				case 3:
 				case 4:
@@ -107,6 +203,7 @@ func main() {
 					fmt.Println("saliendo del menu Clientes...")
 					dormir()
 					l++
+					r = 0
 					limpiar()
 				default:
 					fmt.Println("opcion no valida")
@@ -115,6 +212,7 @@ func main() {
 				}
 
 			} //termina for clientes
+			l = 0 //para que permita regresar al for principal
 
 		case 2:
 			l = 0
@@ -134,7 +232,7 @@ func main() {
 			fmt.Println("opcion no valida")
 			dormir()
 			limpiar()
-		}
+		} //termina swith principal
 
 	}
 
--- prueba.go
+++ prueba.go
@@ -3,22 +3,68 @@ package main
 import (
 	_ "dmnuw"
 	"fmt"
-	"strings"
+	_ "io/ioutil"
+	_ "os"
+	_ "strconv"
+	_ "strings"
 	"time"
 )
 
+/*---type fecha struct {
+	dia int
+	mes time.Month
+	año int
+}*/
+
 func main() {
-	var sl []string
+	/*---	var actual fecha
 	r := time.Now()
-	l := r.String()
-	array := strings.Fields(l)
-	fmt.Println(l)
-	for i, v := range array {
-		if i < 2 {
-			sl = append(sl, v[i])
+	actual.año = r.Year()
+	actual.dia = r.Day()
+	actual.mes = r.Month()
+	d, s := r.Zone()
+	//m["mes"] = int(actual.mes)
+	fmt.Println(actual, d, s)*/
+
+	//otrofile, _ := os.Create("destiny.txt")
+
+	/*
+		para añadir strings a un archivo
+
+		file, _ := os.Create("source.txt")
+		for i := 0; i < 1001; i++ {
+			file.WriteString(strconv.Itoa(i) + "\n")
 		}
+		sl, _ := ioutil.ReadFile("source.txt")
+		//contenido de source en la varible d
+		d := string(sl)
+		d = d + "\n"
+		file.Close()
+		n, _ := os.OpenFile("source.txt", os.O_APPEND, 0777)
+		var st string
+		for i := 1000; i >= 0; i-- {
+			st = st + strconv.Itoa(i) + "\n"
+		}
+		//nw, _ := os.OpenFile("destiny.txt", os.O_APPEND, 0777)
+		d = d + st
+		d = d + "\n"
+		//escribimos el archivo ya con los datos anteriores y con los actuales
+		n.WriteString(d)
+		fmt.Println("success!!")*/
+
+	start := time.Now()
+	s := start.String()
+
+	run := []rune(s)
+	fmt.Println("before", len(run))
+	run = append(run[:0], run[:20]...)
+	fmt.Println("after", len(run))
+	for i, v := range run {
+
+		if i%10 == 0 {
+			fmt.Println()
+		}
+		fmt.Printf("%c", v)
 	}
-	fmt.Println("\n", len(array), "\n\t el nuevo slice es", sl, " y su longitud es",
-		len(sl))
 
 }
